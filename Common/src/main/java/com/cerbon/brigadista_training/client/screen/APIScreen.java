bd724ee82a3922fc4615fbb17c53b1aa5e1ab247
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-05-29 13:07:59 -0300

    Add APIScreen

---
+++ Common/src/main/java/com/cerbon/brigadista_training/client/screen/APIScreen.java
@@ -0,0 +1,60 @@
+package com.cerbon.brigadista_training.client.screen;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class APIScreen extends Screen {
+
+    private EditBox apiBox;
+
+    private Button saveButton;
+
+    private final Minecraft client = Minecraft.getInstance();
+
+    public APIScreen() {
+        super(Component.literal("API Screen"));
+    }
+
+    @Override
+    protected void init() {
+        this.apiBox = new EditBox(this.font, this.width / 2 - 150, this.height / 2 - 20, 300, 20, Component.literal("API Key"));
+
+        this.saveButton = Button.builder(Component.literal("Save"), this::onSave).pos(this.width / 2 - 75, this.height / 2 + 5).width(150).build();
+
+        this.addRenderableWidget(this.apiBox);
+        this.addRenderableWidget(this.saveButton);
+
+        this.setInitialFocus(this.apiBox);
+    }
+
+    @Override
+    public void resize(Minecraft minecraft, int width, int height) {
+        String s = this.apiBox.getValue();
+
+        this.init(minecraft, width, height);
+
+        this.apiBox.setValue(s);
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+
+        guiGraphics.drawString(this.font, Component.literal("API Key"), this.width / 2 - 150, this.height / 2 - 35, 10526880);
+    }
+
+    @Override
+    public boolean shouldCloseOnEsc() {
+        return false;
+    }
+
+    private void onSave(Button button) {
+        if (!apiBox.getValue().isEmpty() && client.screen == this)
+            client.setScreen(null);
+    }
+}
--- NeoForge/src/main/java/com/cerbon/brigadista_training/neoforge/events/NeoClientEvents.java
+++ NeoForge/src/main/java/com/cerbon/brigadista_training/neoforge/events/NeoClientEvents.java
@@ -1,14 +1,31 @@
 package com.cerbon.brigadista_training.neoforge.events;
 
 import com.cerbon.brigadista_training.BrigadistaTraining;
+import com.cerbon.brigadista_training.client.screen.APIScreen;
 import com.cerbon.brigadista_training.neoforge.key_mapping.BDTKeyMappings;
+import com.cerbon.cerbons_api.api.general.event.TimedEvent;
+import com.cerbon.cerbons_api.api.static_utilities.CapabilityUtils;
+import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 @EventBusSubscriber(modid = BrigadistaTraining.MOD_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public class NeoClientEvents {
+
+    @SubscribeEvent
+    public static void onClientJoinWorld(ClientPlayerNetworkEvent.LoggingIn event) {
+        //Add delay because the screen was not showing
+        //TODO: Prevent screen if API key has already being setup
+        CapabilityUtils.getLevelEventScheduler(event.getPlayer().level()).addEvent(
+                new TimedEvent(
+                        () -> Minecraft.getInstance().setScreen(new APIScreen()),
+                        60
+                )
+        );
+    }
 
     @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Post event) {
