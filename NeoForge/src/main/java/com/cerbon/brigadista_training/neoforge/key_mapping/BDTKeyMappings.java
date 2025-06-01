bc520799e52676ba7260ec04b58b0bd8f283f7d8
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-06-01 12:08:04 -0300

    Add keyBind to open info overlay

--- NeoForge/src/main/java/com/cerbon/brigadista_training/neoforge/key_mapping/BDTKeyMappings.java
+++ NeoForge/src/main/java/com/cerbon/brigadista_training/neoforge/key_mapping/BDTKeyMappings.java
@@ -1,5 +1,6 @@
 package com.cerbon.brigadista_training.neoforge.key_mapping;
 
+import com.cerbon.brigadista_training.client.gui.InfoOverlay;
 import com.cerbon.brigadista_training.client.screen.QuizScreen;
 import com.cerbon.cerbons_api.api.static_utilities.MiscUtils;
 import com.mojang.blaze3d.platform.InputConstants;
@@ -10,18 +11,33 @@ import net.neoforged.neoforge.client.settings.KeyConflictContext;
 import org.lwjgl.glfw.GLFW;
 
 public class BDTKeyMappings {
+    private static final String CATEGORY = "Debug Brigadista Training Mod";
+
     public static final Lazy<KeyMapping> OPEN_QUIZ_SCREEN_MAPPING = Lazy.of(
             new KeyMapping(
                     "Quiz Screen",
                     KeyConflictContext.IN_GAME,
                     InputConstants.Type.KEYSYM,
                     GLFW.GLFW_KEY_G,
-                    "Debug Brigadista Training Mod"
+                    CATEGORY
+            )
+    );
+
+    public static final Lazy<KeyMapping> INFO_OVERLAY_MAPPING = Lazy.of(
+            new KeyMapping(
+                    "Info Overlay",
+                    KeyConflictContext.IN_GAME,
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_H,
+                    CATEGORY
             )
     );
 
     public static void handleKeyPressed() {
         if (OPEN_QUIZ_SCREEN_MAPPING.get().consumeClick() && MiscUtils.isDevelopmentEnvironment())
             Minecraft.getInstance().setScreen(new QuizScreen());
+
+        else if (INFO_OVERLAY_MAPPING.get().consumeClick() && MiscUtils.isDevelopmentEnvironment())
+            InfoOverlay.shouldRender = !InfoOverlay.shouldRender;
     }
 }
